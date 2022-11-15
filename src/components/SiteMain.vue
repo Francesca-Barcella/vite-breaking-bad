90c90a454f51be15cc11c3835a8a0a1d6ed67826
Francesca-Barcella <pollon.fra@gmail>
2022-11-15 14:08:44 +0100

    vite-bb- aggiunto tasto select_category

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -14,19 +14,30 @@ export default {
 
 <template>
     <main>
+        <!-- filta per category -->
+        <div class="container">
+            <div class="select_category">
+                <select class="p-2 m-3 rounded-3">
+                    <option value="">Select Category</option>
+                    <option value="">Alive</option>
+                    <option value="">Presumed dead</option>
+                    <option value="">PDeceased</option>
+                </select>
+            </div>
+        </div>
         <!--  <characters /> -->
         <section class="characters">
-            <div class="container p-5 bg-white">
-
-                div.<div class="alert dark" role="alert">
-                    <h4 class="alert-heading"></h4>
-                    <p>Found Characters 62</p>
+            <div class="container bg-white p-3">
+                <!-- numero attori trovati -->
+                <div class="alert dark" role="alert">
+                    <p><strong>Found Characters</strong> 62</p>
                 </div>
-                <div class="row row-cols-1 row-cols-sm-5">
+                <!-- lista attori -->
+                <div class="row row-cols-1 row-cols-md-5 g-4 ">
                     <div class="col" v-for="character in state.characters">
-                        <div class="card character rounded-0 bg-dark text-white text-center p-2">
+                        <div class="card character rounded-0 bg-dark text-white text-center p-3">
                             <img :src="character.img" alt="">
-                            <h3 class="actor">{{ character.name }}</h3>
+                            <h4 class="actor">{{ character.name }}</h4>
                             <span class="category">{{ character.category }}</span>
                             <span class="status">{{ character.status }}</span>
                         </div>
